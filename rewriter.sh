c9b351e8ca81d70f50284bd9c143bf9f9c1c449f
Tom Hardy <wuxmedia@gmail.com>
2016-01-28 09:20:54 +0000

    takes file input now, made read bit more normal

--- rewriter.sh
+++ rewriter.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # crap rewrite mangler
-while read line
-do 
+while IFS='' read -r line || [[ -n "$line" ]]; do
   ID=$(echo "$line" | cut -d "?" -f2 | awk '{print $1}') 
   URL=$(echo "$line"| cut -d "/" -f5)
-echo "RewriteCond %{QUERY_STRING} "$ID"$"
-echo "RewriteRule (.*) /"$URL"? [R=301,L]"
-
-done <rewrite
+  echo "RewriteCond %{QUERY_STRING} "$ID"$"
+  echo "RewriteRule (.*) /"$URL"? [R=301,L]"
+done <"$1"
